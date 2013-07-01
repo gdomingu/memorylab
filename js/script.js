52a02ddc0ea866fa4bc8acdf29de442e181493cb
Gabe <gabedominguez@outlook.com>
2013-07-01 14:19:59 -0700

    added js to show only two pictures at a time

--- js/script.js
+++ js/script.js
@@ -1,54 +1,39 @@
 $(document).ready(function(){
 
-  var photos = $('#puppies li');
-  $.each(photos, function(k, v) {
-    var photo = $(v);
-    var time = $('time', photo);
-    photo.on('mouseover', function(){
-      time.show();
-    });
-    photo.on('mouseout', function(){
-      time.hide();
-    });
-    photo.on('click', function(){
-      var img = photo.find('img');
-      cool_box(img.clone());
-    });
-  });
+  var tiles = $('.tile');
 
+  var num_clicks = 0;
 
-var cool_box = function(img) {
-  if ($(document).width() < 480){
-    return false;
-  }
-  var cb_wrap = $('<div id="cb-wrap"/>');
-  var cb_container = $('<div id="cb-container"/>');
-  var close_btn = $('<button class="close"/>');
-  close_btn.html('&times;');
-  cb_container.append(img);
-  cb_container.append(close_btn);
-  cb_wrap.append(cb_container);
-  $('body').append(cb_wrap);
+  var selected = []
 
-  close_btn.on('click', function(){
-    close();
-  });
-  cb_wrap.on('click', function(event){
-    if (event.target.id == "cb-wrap"){
-    close();}
-
-  });
-
-  $(document).on('keyup', function(event){
-    if (event.keyCode == 27) {
-      close();
+  var handle_click = function (){
+    var tile = $(this);
+    if (tile.hasClass('active')){
+      return false;
     }
+    if (selected.length == 2){
+      $('.tile.active').removeClass('active');
+      selected = [];
+    }
+    num_clicks++;
+    tile.addClass('active');
+    selected.push(tile);
+  }
 
+  $.each(tiles, function(index, tile){
+    var tile = $(tile);
+    tile.on('click', handle_click);
   });
-var close = function(){
-  cb_wrap.remove();
-}
-};
+
+
+
+
+
+
+
+
+
+
 
 
 
